31188c23f2ba2c22c4ddf9fccc3e0f81808a2320
Nicolas-SalasP <nicolas.salas.contacto@gmail.com>
2026-06-17 10:40:42 -0400

    Feat (rrhh): cuentas RRHH en catalogo maestro + RrhhMapeoContableSeeder con mapeo completo para empresa demo

--- Backend-laravel/database/seeders/DatabaseSeeder.php
+++ Backend-laravel/database/seeders/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
             CotizacionSeeder::class,
             RrhhParametrosLegalesSeeder::class,
             TasaRetencionHonorariosSeeder::class,
+            RrhhMapeoContableSeeder::class,
         ]);
     }
 }
